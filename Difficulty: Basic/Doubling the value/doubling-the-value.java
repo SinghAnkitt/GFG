b4ddbd605ee6e1730b714ff634b97a40e2b176f3
Ankit Kumar Singh <148609982+SinghAnkitt@users.noreply.github.com>
2026-09-12 00:00:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Doubling the value/doubling-the-value.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solve(int b, List<Integer> arr) {
+        // code here
+        for(int i = 0; i < arr.size(); i++){
+            if(arr.get(i) == b ) b *= 2;
+        }
+        return b;
+    }
+}
